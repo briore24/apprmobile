fb0cc95df336f33da9f02646fb17e12c418f0b13
Bridget OReilly <bridgetoreilly24@gmail.com>
2025-08-29 13:46:31 -0500

    react cleanup

--- submodule/technician-app/.one-pipeline.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-source $WORKSPACE/$PIPELINE_CONFIG_REPO_PATH/scripts/apps/graphite/.one-pipeline.sh
--- submodule/technician-app/testcases/automation/testcases/src/test/java/com/ibm/maximo/technician/testcases/calibration/support/objects/maximo/CalibrationMaximoObjectInterface.java
+++
@@ -1,8 +0,0 @@
-package com.ibm.maximo.technician.testcases.calibration.support.objects.maximo;
-
-interface CalibrationMaximoObjectInterface {
-
-    public int setup();
-
-    public int teardown();
-}
